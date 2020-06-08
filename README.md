5f64911fba516bc754e3c3afce5c705dbff0b8bb
patricialan <54603331+patricialan@users.noreply.github.com>
2020-06-07 22:10:30 -0600

    adding finalized items for challenge

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # Movies-ETL
+
+# Project Overview
+The purpose of the project was to create an automated extraction, transformation, and loading (ETL) pipeline for new movie data. Datasets would be downloaded from multiple sources (Wikipedia (Wiki), Kaggle.com, and MovieLens), undergo automated ETL, and would be loaded into a PostgreSQL database. See <[challenge.py](challenge.py)> for the automated ETL script. 
+
+# Resources
+Data: <[wikipedia.movies.json](Data/wikipedia.movies.json)>, movies_metadata.csv, and ratings.csv. (The last two files are available <[here](https://www.kaggle.com/rounakbanik/the-movies-dataset/download)>.)
+
+Software: json 2.0.9, numpy 1.18.1, pandas 1.0.3, re 2.2.1, sqlalchemy 1.3.17, jupyter notebook 6.0.3, and python 3.8.2
+
+# Discussion
+To create an automated ETL pipeline, assumptions were made about the datasets.
+
+Some assumptions were that...
+1) Kaggle data would continue to have less outliers, be more consistent in content and data types within a column, and have less null values than Wiki data. The implications are that, even though Kaggle and Wiki share some column headers, only Kaggle data would be used to fill the columns 'title', 'release_date', 'original_language', and 'production_companies'; Kaggle data would be the main source for the columns 'runtime', 'budget', and 'revenue' and null values would be replaced with Wiki data; finally, Wiki data would supply the remaining columns (their data being only available from Wiki).
+
+2) The raw ratings.csv file would continue to have the same table structure and the same data type for each column. This assumption must be true for ratings.csv data to be loaded into the PostgreSQL database since the raw file would be loaded into a pre-existing empty table where columns have pre-assigned data types. 
+
+3) Wiki data for 'Box office', 'Budget', and 'Running time' would continue to be string data type or would have list structure. It is based on this assumption that the ETL script converts lists to strings and then, strings to float data type.
+
+4) The majority of 'Box office' and 'Budget' Wiki data would be able to be parsed into certain forms using regular expressions (e.g. $###.# million, $###.# billion, or $###,###,###). These forms limit extraction of data to North American currencies. Thus, it is also assumed that the majority of 'Box office' and 'Budget' Wiki data would continue to be in USD. 
+
+5) Names of important columns would not change in Wiki and Kaggle raw data. The ELT script references names of important columns for data extraction, data type conversions, and column reordering and renaming. Thus, this assumption needs to hold true for the script to successfully run and load files. For Wiki data, important columns are, in general, those with < 90% null rows (since otherwise, columns were discarded). A few examples of names of important columns are 'imdb_link', 'Box office', 'Budget', and 'Running time' in Wiki data and 'id', 'release_date', 'revenue', 'budget', and 'runtime' in Kaggle data. 
---
+++ challenge.py
@@ -0,0 +1,247 @@
+# import dependencies
+import json
+import pandas as pd
+import numpy as np
+import re
+from sqlalchemy import create_engine
+from config import db_password
+import time
+
+# arguments for movies_ETL function
+# replace file paths with your unique file paths
+wiki_path = '/Users/patricialan/Desktop/Module8ETL/Movies-ETL/Data/wikipedia.movies.json'
+kaggle_path = '/Users/patricialan/Desktop/Module8ETL/Movies-ETL/Data/movies_metadata.csv'
+ratings_path = '/Users/patricialan/Desktop/Module8ETL/Movies-ETL/Data/ratings.csv'
+
+# movies_ETL function
+def movies_ETL(wiki_path, kaggle_path, ratings_path):  
+    
+    try:
+        # read files
+        with open(f'{wiki_path}', mode='r') as file:
+            wiki = json.load(file)
+        kaggle = pd.read_csv(f'{kaggle_path}', low_memory = False)
+        ratings = pd.read_csv(f'{ratings_path}')
+    except:
+        print("Unable to read files. Ensure correct file paths.")
+    
+    # ------------------wiki----------------------
+    
+    # movies must have director, imdb link, and not be TV series
+    wiki = [movie for movie in wiki if ('Director' in movie or 'Directed by' in movie) \
+        and 'imdb_link' in movie and 'No. of episodes' not in movie]
+    
+    # change column names
+    def change_column_name(old_name, new_name):         
+        for movie in wiki:
+            if old_name in movie:
+                movie[new_name] = movie.pop(old_name)
+                
+    change_column_name('Adaptation by', 'Writer(s)')
+    change_column_name('Country of origin', 'Country')
+    change_column_name('Directed by', 'Director')
+    change_column_name('Distributed by', 'Distributor')
+    change_column_name('Edited by', 'Editor(s)')
+    change_column_name('Length', 'Running time')
+    change_column_name('Original release', 'Release date')
+    change_column_name('Music by', 'Composer(s)')
+    change_column_name('Produced by', 'Producer(s)')
+    change_column_name('Producer', 'Producer(s)')
+    change_column_name('Productioncompanies ', 'Production company(s)')
+    change_column_name('Productioncompany ', 'Production company(s)')
+    change_column_name('Released', 'Release Date')
+    change_column_name('Release Date', 'Release date')
+    change_column_name('Screen story by', 'Writer(s)')
+    change_column_name('Screenplay by', 'Writer(s)')
+    change_column_name('Story by', 'Writer(s)')
+    change_column_name('Theme music composer', 'Composer(s)')
+    change_column_name('Written by', 'Writer(s)')
+
+    # create dataframe
+    wiki_df = pd.DataFrame(wiki)
+    
+    # new 'imdb_id' column
+    wiki_df['imdb_id'] = wiki_df['imdb_link'].str.extract(r'(tt\d{7})')
+    
+    # drop duplicate imdb_ids
+    wiki_df.drop_duplicates(subset='imdb_id', inplace=True)
+    
+    # keep columns where < 90% of rows are null
+    wiki_columns_to_keep = [column for column in wiki_df.columns if wiki_df[column].isnull().sum() < len(wiki_df)*0.9]
+    wiki_df = wiki_df[wiki_columns_to_keep]
+    
+    # clean columns 'Box office', 'Budget', & 'Running time'
+    ## join lists
+    box_office = wiki_df['Box office'].dropna().apply(lambda x: ' '.join(x) if type(x) == list else x)
+    budget = wiki_df['Budget'].dropna().apply(lambda x: ' '.join(x) if type(x) == list else x)
+    running_time = wiki_df['Running time'].dropna().apply(lambda x: ' '.join(x) if type(x) == list else x)
+    
+    ## format amounts in ranges
+    box_office = box_office.str.replace(r'\$.*[-—–](?![a-z])', '$', regex=True)
+    budget = budget.str.replace(r'\$.*[-—–](?![a-z])', '$', regex=True)
+    
+    ## format amounts with citation references
+    budget = budget.str.replace(r'\[\d+\]\s*', '')
+    
+    ## box office & budget: forms to extract
+    form_one = r'\$\s*\d+\.?\d*\s*[mb]illi?on'
+    form_two = r'\$\s*\d{1,3}(?:[,\.]\d{3})+(?!\s[mb]illion)'
+    
+    ## function converts strings to floating-point numbers
+    def parse_dollars(s):
+        # if s is not a string, return NaN
+        if type(s) != str:
+            return np.nan
+
+        # if input is of the form $###.# million
+        if re.match(r'\$\s*\d+\.?\d*\s*milli?on', s, flags=re.IGNORECASE):
+            # remove dollar sign and " million"
+            s = re.sub('\$|\s|[a-zA-Z]','', s)
+            # convert to float and multiply by a million
+            value = float(s) * 10**6
+            # return value
+            return value
+
+        # if input is of the form $###.# billion
+        elif re.match(r'\$\s*\d+\.?\d*\s*billi?on', s, flags=re.IGNORECASE):
+            # remove dollar sign and " billion"
+            s = re.sub('\$|\s|[a-zA-Z]','', s)
+            # convert to float and multiply by a billion
+            value = float(s) * 10**9
+            # return value
+            return value
+
+        # if input is of the form $###,###,###
+        elif re.match(r'\$\s*\d{1,3}(?:[,\.]\d{3})+(?!\s[mb]illion)', s, flags=re.IGNORECASE):
+            # remove dollar sign and commas
+            s = re.sub('\$|,','', s)
+            # convert to float
+            value = float(s)
+            # return value
+            return value
+
+        # otherwise, return NaN
+        else:
+            return np.nan
+    
+    ## box office & budget: extract forms & apply parse_dollars function
+    wiki_df['box_office'] = box_office.str.extract(f'({form_one}|{form_two})', flags=re.IGNORECASE)[0].apply(parse_dollars)
+    wiki_df['budget'] = budget.str.extract(f'({form_one}|{form_two})', flags=re.IGNORECASE)[0].apply(parse_dollars)
+
+    ## running time:
+    ## extract rows with hours & min
+    running_time_extract = running_time.str.extract(r'(\d+)\s*ho?u?r?s?\s*(\d*)|(\d+)\s*m')
+    ## convert strings to numbers; coerce empties to NaN, then turn into zero
+    running_time_extract = running_time_extract.apply(lambda col: pd.to_numeric(col, errors='coerce')).fillna(0)
+    ## if pure minutes is zero, then convert hours+min to min
+    wiki_df['running_time'] = running_time_extract.apply(lambda row: row[0]*60 + row[1] if row[2] == 0 else row[2], axis=1)
+    
+    # drop old columns
+    wiki_df.drop(['Box office', 'Budget', 'Running time'], axis=1, inplace=True)
+    
+    # ------------------kaggle----------------------
+    
+    try:
+        # keep rows where adult = False (also drops 3 bad data rows) & drop 'adult' column
+        # drop 'video' column because contains 1 unique value "False"
+        kaggle = kaggle[kaggle['adult'] == 'False'].drop(['adult','video'],axis='columns')
+    except:
+        print("Kaggle 'video' column may no longer exist so it can't be dropped. Continuing...")
+    
+    try:
+        # convert columns to numeric dtype
+        kaggle['budget'] = kaggle['budget'].astype(int, errors='raise')
+        kaggle['id'] = pd.to_numeric(kaggle['id'], errors='raise')
+        kaggle['popularity'] = pd.to_numeric(kaggle['popularity'], errors='raise')
+        # convert release date to datetime
+        kaggle['release_date'] = pd.to_datetime(kaggle['release_date'], errors='raise') 
+    except:
+        print("Problems converting Kaggle 'budget', 'id', or 'popularity' to numerical values, or 'release_date' to datetime. Continuing...")
+    
+    # --------------wiki-kaggle merge---------------
+    
+    # merge & rename redundant columns
+    movies_df = pd.merge(wiki_df, kaggle, on='imdb_id', suffixes=['_wiki','_kaggle'])
+    
+    # drop redundant columns: title_wiki, Release date (wiki), Language (wiki), and Production company(s) (wiki)
+    movies_df.drop(columns=['title_wiki', 'Release date', 'Language', 'Production company(s)'], inplace=True)
+    
+    try:
+        # drop a known outlier: tt00457939 titled 'From Here To Eternity' released 1953 
+        # got mixed in same row as 'The Holiday' released 2006
+        movies_df = movies_df.drop(movies_df[(movies_df['url'] == 'https://en.wikipedia.org/wiki/The_Holiday') & (movies_df['imdb_id'] == 'tt0045793')].index)
+    except:
+        print('Corrupt entry for "tt00457939" is no longer present.')
+    
+    # function fills in missing data for a wiki-kaggle column pair, then drops the redundant column
+    def fill_missing_kaggle_data(df, kaggle_column, wiki_column):
+        df[kaggle_column] = df.apply(lambda row: row[wiki_column] if row[kaggle_column] == 0 else row[kaggle_column], axis=1)
+        df.drop(columns=wiki_column, inplace=True)
+        
+    # kaggle columns 'runtime', 'budget_kaggle', & 'revenue': fill with wiki, then drop wiki columns
+    fill_missing_kaggle_data(movies_df, 'runtime', 'running_time')
+    fill_missing_kaggle_data(movies_df, 'budget_kaggle', 'budget_wiki')
+    fill_missing_kaggle_data(movies_df, 'revenue', 'box_office')
+    
+    # reorder columns
+    movies_df = movies_df.loc[:, ['imdb_id','id','title_kaggle','original_title','tagline','belongs_to_collection',
+        'url','imdb_link','runtime','budget_kaggle','revenue','release_date','popularity','vote_average','vote_count',
+        'genres','original_language','overview','spoken_languages','Country','production_companies',
+        'production_countries','Distributor','Producer(s)','Director','Starring','Cinematography','Editor(s)',
+        'Writer(s)','Composer(s)','Based on']]
+    
+    # rename columns
+    movies_df.rename({'id':'kaggle_id',
+        'title_kaggle':'title',
+        'url':'wikipedia_url',
+        'budget_kaggle':'budget',
+        'Country':'country',
+        'Distributor':'distributor',
+        'Producer(s)':'producers',
+        'Director':'director',
+        'Starring':'starring',
+        'Cinematography':'cinematography',
+        'Editor(s)':'editors',
+        'Writer(s)':'writers',
+        'Composer(s)':'composers',
+        'Based on':'based_on'}, axis='columns', inplace=True)
+    
+    #----------load files into PostgreSQL database 'movie_data'---------
+    
+    # create database engine for PostgreSQL to connect
+    db_string = f"postgres://postgres:{db_password}@127.0.0.1:5432/movie_data"
+    engine = create_engine(db_string)
+    
+    try:
+        # load movies_df to PostgreSQL
+        movies_df.to_sql(name='movies', con=engine, if_exists='append')
+    except:
+        print("Unable to load 'movies_df'. Continuing with loading of 'ratings.csv'.")
+    
+    try:
+        # load raw ratings data to PostgreSQL     
+        ## create a variable for the number of rows imported
+        rows_imported = 0
+
+        ## get the start time from time.time()
+        start_time = time.time()
+
+        for data in pd.read_csv(f'{ratings_path}', chunksize=1000000):
+
+            ## print out the range of rows that are being imported
+            print(f'importing rows {rows_imported} to {rows_imported + len(data)}...', end='')
+            data.to_sql(name='ratings', con=engine, if_exists='append')
+
+            ## increment the number of rows imported by the chunksize
+            rows_imported += len(data)
+
+            ## print that the rows have finished importing
+            ## add elapsed time until this print out
+            print(f'Done. {time.time() - start_time} total seconds elapsed')
+    except:
+        print("Unable to load 'ratings.csv'.")
+              
+    return
+
+# call on movies_ETL function
+movies_ETL(wiki_path, kaggle_path, ratings_path)
